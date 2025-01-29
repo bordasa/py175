6b9531355a104e82ec57cc12ecb298ee807843e1
Alexei Bordas <alexei.bordas@gmail.com>
2025-01-29 12:48:40 -0800

    Completed and Refactored the Validations

--- lesson4/todo_starter/app.py
+++ lesson4/todo_starter/app.py
@@ -1,6 +1,24 @@
-from flask import Flask, render_template, url_for, redirect
+from uuid import uuid4
+
+from flask import (
+    flash,
+    Flask,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+
+from todos.utils import error_for_list_title
 
 app = Flask(__name__)
+app.secret_key='secret1'
+
+@app.before_request
+def initialize_session():
+    if 'lists' not in session:
+        session['lists'] = []
 
 @app.route("/")
 def index():
@@ -14,11 +32,26 @@ def add_todo_list():
 # Render the list of todo lists
 @app.route("/lists")
 def get_lists():
-    lists = [
-        {"title": "Lunch Groceries", "todos":[]},
-        {"title": "Dinner Groceries", "todos": []},
-    ]
-    return render_template('lists.html', lists=lists)
+    return render_template('lists.html', lists=session['lists'])
+
+# Create a new todo list
+@app.route("/lists", methods=["POST"])
+def create_list():
+    title = request.form["list_title"].strip()
+    error = error_for_list_title(title, session['lists'])
+    if error:
+        flash(error, "error")
+        return render_template('new_list.html', title=title)
+    
+    session['lists'].append({
+            'id': str(uuid4()),
+            'title': title,
+            'todos': [],
+            })
+
+    flash("The list has been created.", "success")
+    session.modified = True
+    return redirect(url_for('get_lists'))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
